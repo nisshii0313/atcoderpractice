4a766c412106f75b8250f969f79f9100398dda42
nisshii0313 <yuta19990313@gmail.com>
2019-12-09 10:14:06 +0900

    使い易いように短くまとめた

---
+++ standard_input.rb
@@ -0,0 +1,16 @@
+# 1行1要素のみ
+line = gets.chomp #文字列
+line = gets.chomp.to_i #数値
+
+# 1行複数要素
+line = gets.split(' ') #文字列
+line = gets.split(' ').map(&:to_i) #数値
+
+# 複数行1要素のみ
+line = readlines.map(&:chomp) #文字列
+line = readlines.map(&:chomp).map(&:to_i) #数値
+
+# 複数行複数要素
+lines = readlines(chomp: true).map{|line| line.split(' ')} #文字列
+lines = readlines(chomp: true).map{|line| line.split(' ').map(&:to_i)} #数値
+
